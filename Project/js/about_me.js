3991815de0305f6906fad7fc3096e2d37c86dcf5
keenanfarhan <125755226+keenanfarhan@users.noreply.github.com>
2023-04-15 13:05:33 -0400

    Add files via upload

--- Project/js/about_me.js
+++ Project/js/about_me.js
@@ -4,6 +4,6 @@ const Keenan = {
     school: "mount olive high school",
     grade: "9",
     favorite_subject: "social studies",
-    image_url: "https://drive.google.com/file/d/18u4E1WGZUMTBglZ9yhRoeBoyguLCQ0mS/view",
+    image_url: "https://drive.google.com/uc?export=download&id=18u4E1WGZUMTBglZ9yhRoeBoyguLCQ0mS",
     home_page_url: "file:///C:/Users/keen4/OneDrive/Documents/GitHub/RWCWebDevelopment/Project/index.html"
 }
