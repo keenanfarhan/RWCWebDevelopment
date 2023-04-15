108948b89361c395fb758e831144f3fa54aaefb3
keenanfarhan <125755226+keenanfarhan@users.noreply.github.com>
2023-04-15 12:45:41 -0400

    Add files via upload

---
+++ Project/js/about_me.js
@@ -0,0 +1,10 @@
+const Keenan = {
+    first_name: "keenan",
+    last_name: "farhan",
+    age: "14",
+    school: "mount olive high school",
+    grade: "9",
+    favorite_subject: "social studies",
+    image_location: "https://drive.google.com/file/d/18u4E1WGZUMTBglZ9yhRoeBoyguLCQ0mS/view",
+    site: "file:///C:/Users/keen4/OneDrive/Documents/GitHub/RWCWebDevelopment/Project/index.html"
+}
